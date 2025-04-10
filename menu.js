9793ddd717f8a0aa8ed52c8ec8fdb7fb1fcb25bb
Felipe <valdirfelipe_@Outlook.com>
2025-04-10 20:53:16 -0300

    add tt

--- menu.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const navSite = document.querySelector('.nav-site');
-
-menuToggle.addEventListener('click', () => {
-  navSite.classList.toggle('active');
-});
